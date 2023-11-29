914f7ad9b22fd5bcf607273e85895af263d6e5c5
Liang Zhang <psychelzh@outlook.com>
2023-11-30 00:03:09 +0800

    Added region and trial info to output
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- representation.m
+++ representation.m
@@ -2,23 +2,25 @@ channel = readtable(fullfile("config", "eeg_channel_labels_64.csv"), TextType="s
 subjs_id = unique(readtable(fullfile("data", "group_task-wordencoding_events.csv")).subj_id, "stable");
 load(fullfile("data", "grp_subjs206_nodemean_1000ms.mat"), "grp_data");
 [~, len_time_point, len_trial, len_sub] = size(grp_data);
-region_id = 1:6;
+regions_id = 1:6;
 
 % debug
-% region_id = 1:2;
+% regions_id = 1:2;
 % len_trial = 2;
 
 %% intersubject similarity
+[regions_id_idx, trial_id_idx] = meshgrid(regions_id, 1:len_trial);
+numel_region_trial = numel(regions_id_idx);
 numel_cor_mat = len_sub * len_sub;
 [sub_row, sub_col] = ind2sub([len_sub, len_sub], 1:(numel_cor_mat));
-simi_inter_subj = repmat(array2table( ...
-    [subjs_id(sub_row), subjs_id(sub_col), nan(len_sub * len_sub, 1)], ...
-    "VariableNames", ["subj_id_x", "subj_id_y", "r"]), ...
-    length(region_id) * len_trial, 1);
+simi_inter_subj = array2table(...
+    [repelem([regions_id_idx(:), trial_id_idx(:)], numel_cor_mat, 1), ...
+    repmat([subjs_id(sub_row), subjs_id(sub_col), nan(numel_cor_mat, 1)], numel_region_trial, 1)], ...
+    "VariableNames", ["region_id", "trial_id", "subj_id_row", "subj_id_col", "r"]);
 % easier handle data store
 r_start = 1;
 r_end = numel_cor_mat;
-for i_region = region_id
+for i_region = regions_id
     chan_in_reg = channel.code(channel.("region" + string(i_region)) ~= 0);
     for i_trial = 1:len_trial
         % collapse channel and time (thus spatiotemporal pattern)
@@ -30,8 +32,9 @@ for i_region = region_id
         r_end = r_end + numel_cor_mat;
     end
 end
-parquetwrite(fullfile("data", "type-inter_rsa.parquet"), simi_inter_subj)
-
+parquetwrite(fullfile("data", "task-rs_acq-inter_type-full.parquet"), simi_inter_subj)
+parquetwrite(fullfile("data", "task-rs_acq-inter_type-triu.parquet"), ...
+    simi_inter_subj(simi_inter_subj.subj_id_row < simi_inter_subj.subj_id_col, :))
 
 %% individual to group similarity
 
