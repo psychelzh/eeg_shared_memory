32a2702520dd786f7c2106221763003e22083cc7
Liang Zhang <psychelzh@outlook.com>
2023-11-30 00:42:08 +0800

    Add individual to group similarity
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ +utils/calc_simi_ind_to_grp.m
@@ -0,0 +1,14 @@
+function simi = calc_simi_ind_to_grp(dat)
+%CALC_SIMI_IND_TO_GRP Calculate individual to group similarity
+%   The group representation is calcualted by averaging all the subjects
+%   except the current calculating subject.
+
+ncols = size(dat, 2);
+simi = nan(ncols, 1);
+for i_col = 1:ncols
+    cur_rep = dat(:, i_col);
+    grp_rep = mean(dat(:, setdiff(1:ncols, i_col)), 2, "omitmissing");
+    simi(i_col) = corr(cur_rep, grp_rep);
+end
+end
+
--- representation.m
+++ representation.m
@@ -1,21 +1,23 @@
 channel = readtable(fullfile("config", "eeg_channel_labels_64.csv"), TextType="string");
 subjs_id = unique(readtable(fullfile("data", "group_task-wordencoding_events.csv")).subj_id, "stable");
 load(fullfile("data", "grp_subjs206_nodemean_1000ms.mat"), "grp_data");
-[~, len_time_point, len_trial, len_sub] = size(grp_data);
+[~, len_time_point, len_trial, len_subj] = size(grp_data);
 regions_id = 1:6;
 
 % debug
 % regions_id = 1:2;
 % len_trial = 2;
 
-%% intersubject similarity
+% used to construct table pre-allocation
 [regions_id_idx, trial_id_idx] = meshgrid(regions_id, 1:len_trial);
 numel_region_trial = numel(regions_id_idx);
-numel_cor_mat = len_sub * len_sub;
-[sub_row, sub_col] = ind2sub([len_sub, len_sub], 1:(numel_cor_mat));
+
+%% intersubject similarity
+numel_cor_mat = len_subj * len_subj;
+[subj_row, subj_col] = ind2sub([len_subj, len_subj], 1:(numel_cor_mat));
 simi_inter_subj = array2table(...
     [repelem([regions_id_idx(:), trial_id_idx(:)], numel_cor_mat, 1), ...
-    repmat([subjs_id(sub_row), subjs_id(sub_col), nan(numel_cor_mat, 1)], numel_region_trial, 1)], ...
+    repmat([subjs_id(subj_row), subjs_id(subj_col), nan(numel_cor_mat, 1)], numel_region_trial, 1)], ...
     "VariableNames", ["region_id", "trial_id", "subj_id_row", "subj_id_col", "r"]);
 % easier handle data store
 r_start = 1;
@@ -25,7 +27,7 @@ for i_region = regions_id
     for i_trial = 1:len_trial
         % collapse channel and time (thus spatiotemporal pattern)
         cur_dat = reshape(grp_data(chan_in_reg, :, i_trial, :), ...
-            [length(chan_in_reg) * len_time_point, len_sub]);
+            [length(chan_in_reg) * len_time_point, len_subj]);
         cur_cor_mat = corr(cur_dat);
         simi_inter_subj.r(r_start:r_end) = cur_cor_mat(:);
         r_start = r_start + numel_cor_mat;
@@ -37,4 +39,22 @@ parquetwrite(fullfile("data", "task-rs_acq-inter_type-triu.parquet"), ...
     simi_inter_subj(simi_inter_subj.subj_id_row < simi_inter_subj.subj_id_col, :))
 
 %% individual to group similarity
-
+simi_ind_to_grp = array2table(...
+    [repelem([regions_id_idx(:), trial_id_idx(:)], len_subj, 1), ...
+    repmat([subjs_id, nan(len_subj, 1)], numel_region_trial, 1)], ...
+    "VariableNames", ["region_id", "trial_id", "subj_id", "r"]);
+% easier handle data store
+r_start = 1;
+r_end = len_subj;
+for i_region = regions_id
+    chan_in_reg = channel.code(channel.("region" + string(i_region)) ~= 0);
+    for i_trial = 1:len_trial
+        % collapse channel and time (thus spatiotemporal pattern)
+        cur_dat = reshape(grp_data(chan_in_reg, :, i_trial, :), ...
+            [length(chan_in_reg) * len_time_point, len_subj]);
+        simi_ind_to_grp.r(r_start:r_end) = utils.calc_simi_ind_to_grp(cur_dat);
+        r_start = r_start + len_subj;
+        r_end = r_end + len_subj;
+    end
+end
+parquetwrite(fullfile("data", "task-rs_acq-group.parquet"), simi_ind_to_grp)
