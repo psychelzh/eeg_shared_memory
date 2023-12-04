53c9e37152e2fac94959579d2f17c09107f466af
Liang Zhang <psychelzh@outlook.com>
2023-12-04 20:35:10 +0800

    Use ` combinations()`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- representation.m
+++ representation.m
@@ -8,21 +8,17 @@ regions_id = 1:6;
 % regions_id = 1:2;
 % len_trial = 2;
 
-% used to construct table pre-allocation
-[regions_id_idx, trial_id_idx] = meshgrid(regions_id, 1:len_trial);
-numel_region_trial = numel(regions_id_idx);
-
 %% intersubject similarity
 
-numel_cor_mat = len_subj * len_subj;
-[subj_row, subj_col] = ind2sub([len_subj, len_subj], 1:(numel_cor_mat));
-
 % acquire: trial-level
-simi_inter_by_trial = array2table(...
-    [repelem([regions_id_idx(:), trial_id_idx(:)], numel_cor_mat, 1), ...
-    repmat([subjs_id(subj_row), subjs_id(subj_col), nan(numel_cor_mat, 1)], numel_region_trial, 1)], ...
-    "VariableNames", ["region_id", "trial_id", "subj_id_row", "subj_id_col", "fisher_z"]);
+simi_inter_by_trial = ...
+    combinations(regions_id, 1:len_trial, subjs_id, subjs_id);
+simi_inter_by_trial.Properties.VariableNames = ...
+    ["region_id", "trial_id", "subj_id_col", "subj_id_row"];
+simi_inter_by_trial.fisher_z = nan(height(simi_inter_by_trial), 1);
+
 % easier handle data store
+numel_cor_mat = len_subj * len_subj;
 store_start = 1;
 store_end = numel_cor_mat;
 fprintf("Processing trial level intersubject similarity...\n")
@@ -33,8 +29,8 @@ for i_region = regions_id
         % collapse channel and time (thus spatiotemporal pattern)
         cur_dat = reshape(grp_data(chan_in_reg, :, i_trial, :), ...
             [length(chan_in_reg) * len_time_point, len_subj]);
-        cur_cor_mat = atanh(corr(cur_dat));
-        simi_inter_by_trial.fisher_z(store_start:store_end) = cur_cor_mat(:);
+        cur_cor_mat = corr(cur_dat);
+        simi_inter_by_trial.fisher_z(store_start:store_end) = atanh(cur_cor_mat(:));
         store_start = store_start + numel_cor_mat;
         store_end = store_end + numel_cor_mat;
         prog_bar([], [], [])
@@ -45,10 +41,11 @@ parquetwrite(fullfile("data", "type-inter_acq-trial_rs.parquet"), ...
     simi_inter_by_trial(simi_inter_by_trial.subj_id_row < simi_inter_by_trial.subj_id_col, :))
 
 % acquire: whole-time-series
-simi_inter_by_whole = array2table(...
-    [repelem(regions_id(:), numel_cor_mat, 1), ...
-    repmat([subjs_id(subj_row), subjs_id(subj_col), nan(numel_cor_mat, 1)], length(regions_id), 1)], ...
-    "VariableNames", ["region_id", "subj_id_row", "subj_id_col", "fisher_z"]);
+simi_inter_by_whole = combinations(regions_id, subjs_id, subjs_id);
+simi_inter_by_whole.Properties.VariableNames = ...
+    ["region_id", "subj_id_col", "subj_id_row"];
+simi_inter_by_whole.fisher_z = nan(height(simi_inter_by_whole), 1);
+
 store_start = 1;
 store_end = numel_cor_mat;
 fprintf("Processing whole time series intersubject similarity...\n")
@@ -56,8 +53,8 @@ for i_region = progress(regions_id)
     chan_in_reg = channel.code(channel.("region" + string(i_region)) ~= 0);
     cur_dat = reshape(grp_data(chan_in_reg, :, :, :), ...
         [length(chan_in_reg) * len_time_point * len_trial, len_subj]);
-    cur_cor_mat = atanh(corr(cur_dat, rows="pairwise"));
-    simi_inter_by_whole.fisher_z(store_start:store_end) = cur_cor_mat(:);
+    cur_cor_mat = corr(cur_dat, rows="pairwise");
+    simi_inter_by_whole.fisher_z(store_start:store_end) = atanh(cur_cor_mat(:));
     store_start = store_start + numel_cor_mat;
     store_end = store_end + numel_cor_mat;
 end
@@ -67,10 +64,10 @@ parquetwrite(fullfile("data", "type-inter_acq-whole_rs.parquet"), ...
 %% individual to group similarity
 
 % acquire: trial-level
-simi_grp_by_trial = array2table(...
-    [repelem([regions_id_idx(:), trial_id_idx(:)], len_subj, 1), ...
-    repmat([subjs_id, nan(len_subj, 1)], numel_region_trial, 1)], ...
-    "VariableNames", ["region_id", "trial_id", "subj_id", "fisher_z"]);
+simi_grp_by_trial = combinations(regions_id, 1:len_trial, subjs_id);
+simi_grp_by_trial.Properties.VariableNames = ["region_id", "trial_id", "subj_id"];
+simi_grp_by_trial.fisher_z = nan(height(simi_grp_by_trial), 1);
+
 % easier handle data store
 store_start = 1;
 store_end = len_subj;
@@ -93,10 +90,10 @@ end
 parquetwrite(fullfile("data", "type-group_acq-trial_rs.parquet"), simi_grp_by_trial)
 
 % acquire: whole-time-series
-simi_grp_by_whole = array2table(...
-    [repelem(regions_id(:), len_subj, 1), ...
-    repmat([subjs_id, nan(len_subj, 1)], length(regions_id), 1)], ...
-    "VariableNames", ["region_id", "subj_id", "fisher_z"]);
+simi_grp_by_whole = combinations(regions_id, subjs_id);
+simi_grp_by_whole.Properties.VariableNames = ["region_id", "subj_id"];
+simi_grp_by_whole.fisher_z = nan(height(simi_grp_by_whole), 1);
+
 store_start = 1;
 store_end = len_subj;
 fprintf("Processing whole time series individual to group similarity...\n")
