bd8120527b3c50a65f9d65cbe51e4b1c71e4a86e
Liang Zhang <psychelzh@outlook.com>
2023-12-16 17:43:13 +0800

    Remove one obsolete function
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/targets.R
+++ R/targets.R
@@ -1,33 +1,3 @@
-tar_summary_with_branches <- function(name, data, .by) {
-  name <- deparse1(substitute(name))
-  list(
-    tar_target_raw(
-      sprintf("%s_branches", name),
-      bquote(
-        .(substitute(data)) |>
-          lapply(
-            summarise,
-            n = n(),
-            sum_fisher_z = sum(fisher_z),
-            .by = .(substitute(.by))
-          ) |>
-          list_rbind()
-      ),
-      pattern = bquote(map(.(substitute(data))))
-    ),
-    tar_target_raw(
-      name,
-      bquote(
-        .(as.symbol(sprintf("%s_branches", name))) |>
-          summarise(
-            mean_fisher_z = sum(sum_fisher_z) / sum(n),
-            .by = .(substitute(.by))
-          )
-      )
-    )
-  )
-}
-
 tar_combine_with_meta <- function(name, targets, cols_targets,
                                   prefix = NULL,
                                   fun_pre = NULL,
