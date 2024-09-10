28dfb6d0f84e8c4f85a00813efb60cc0e3f3f536
Liang Zhang <psychelzh@outlook.com>
2024-09-10 20:04:57 +0800

    Complete ISS part
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/analyze_inter_subject.R
+++ _scripts/analyze_inter_subject.R
@@ -4,7 +4,7 @@ tar_option_set(
   packages = c("tidyverse"),
   controller = crew::crew_controller_local(
     name = "local",
-    workers = 8
+    workers = 12
   ),
   garbage_collection = TRUE,
   memory = "transient",
@@ -25,11 +25,52 @@ calc_iss <- function(patterns_cca, pattern_semantics) {
     )
 }
 
-calc_iss_stats <- function(data) {
+calc_iss_stats <- function(data, ..., .by = c(cca_id, time_id)) {
   data |>
     summarise(
-      broom::tidy(t.test(iss, alternative = "greater")),
-      .by = c(cca_id, time_id)
+      broom::tidy(t.test(iss, ...)),
+      .by = {{ .by }}
+    )
+}
+
+calc_clusters_stats <- function(stats, stats_permuted,
+                                by = "cca_id",
+                                col_statistic = statistic,
+                                col_p_value = p.value,
+                                col_time_id = time_id,
+                                col_id_permuted = starts_with("tar")) {
+  clusters <- stats |>
+    reframe(
+      find_cluster(
+        {{ col_statistic }},
+        {{ col_p_value }},
+        {{ col_time_id }}
+      ),
+      .by = all_of(by)
+    )
+  clusters_permuted <- stats_permuted |>
+    reframe(
+      find_cluster(
+        {{ col_statistic }},
+        {{ col_p_value }},
+        {{ col_time_id }},
+        keep = "largest"
+      ),
+      .by = c(all_of(by), {{ col_id_permuted }})
+    )
+  clusters |>
+    left_join(
+      clusters_permuted |>
+        select(cca_id, cluster_mass_perm = cluster_mass) |>
+        chop(cluster_mass_perm),
+      by = by
+    ) |>
+    mutate(
+      p_perm = map2_dbl(
+        cluster_mass_perm,
+        cluster_mass,
+        ~ (sum(.x >= .y) + 1) / (length(.x) + 1)
+      )
     )
 }
 
@@ -65,6 +106,16 @@ find_cluster <- function(statistic, p.value,
     clusters <- slice_max(clusters, cluster_mass)
   }
   clusters
+}
+
+convert_p2_p1 <- function(statistic, p.value,
+                          alternative = c("greater", "less")) {
+  alternative <- match.arg(alternative)
+  ifelse(
+    xor(alternative == "greater", statistic > 0),
+    1 - p.value / 2,
+    p.value / 2
+  )
 }
 
 list(
@@ -115,17 +166,58 @@ list(
       column_to_rownames("trial_id") |>
       proxy::simil(method = "cosine")
   ),
-  tar_target(iss, calc_iss(patterns_cca, pattern_semantics)),
-  tar_target(iss_stats, calc_iss_stats(iss)),
+  tar_target(data_iss, calc_iss(patterns_cca, pattern_semantics)),
+  tar_target(iss_stats, calc_iss_stats(data_iss)),
   tarchetypes::tar_rep(
-    iss_stats_permuted,
-    calc_iss_stats(
-      calc_iss(
-        patterns_cca,
-        seriation::permute(pattern_semantics, sample.int(150L))
-      )
+    data_iss_permuted,
+    calc_iss(
+      patterns_cca,
+      seriation::permute(pattern_semantics, sample.int(150L))
     ),
     reps = 10,
-    batches = 100
+    batches = 100,
+    iteration = "list"
+  ),
+  tarchetypes::tar_rep2(
+    iss_stats_permuted,
+    calc_iss_stats(data_iss_permuted, alternative = "greater"),
+    data_iss_permuted
+  ),
+  tar_target(
+    clusters_stats_iss,
+    iss_stats |>
+      mutate(p.value = convert_p2_p1(statistic, p.value)) |>
+      calc_clusters_stats(iss_stats_permuted)
+  ),
+  tar_target(
+    patterns_cca_whole,
+    arrow::open_dataset(file_cca_y) |>
+      filter(time_id >= 51) |>
+      collect() |>
+      pivot_wider(names_from = trial_id, values_from = y) |>
+      summarise(
+        pattern_cca = list(as.dist(cor(pick(matches("^\\d+$"))))),
+        .by = c(subj_id, cca_id)
+      )
+  ),
+  tar_target(data_iss_whole, calc_iss(patterns_cca_whole, pattern_semantics)),
+  tar_target(iss_stats_whole, calc_iss_stats(data_iss_whole, .by = cca_id)),
+  tar_target(
+    iss_comparison,
+    data_iss_whole |>
+      mutate(cca_id = factor(cca_id)) |>
+      lmerTest::lmer(iss ~ cca_id + (1 | subj_id), data = _) |>
+      emmeans::emmeans(
+        ~ cca_id,
+        lmer.df = "satterthwaite",
+        lmerTest.limit = Inf
+      ) |>
+      emmeans::contrast("pairwise") |>
+      broom::tidy() |>
+      separate_wider_delim(
+        contrast, " - ",
+        names = c("start", "end")
+      ) |>
+      mutate(across(c("start", "end"), parse_number))
   )
 )
