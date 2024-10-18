243492b12c1392ff2c493c99e41d699208afb11f
Liang Zhang <psychelzh@outlook.com>
2024-10-18 17:35:29 +0800

    Enhance behavioral scores calculation

--- R/behav.R
+++ R/behav.R
@@ -1,8 +1,10 @@
 clean_events <- function(file, subjs) {
   read_tsv(file, show_col_types = FALSE) |>
     mutate(subj_id = match(subj, subjs)) |>
+    # drop trials without response
     filter(resp != 0, !is.na(subj_id)) |>
-    mutate(acc = resp != 0 & xor(old_new == 1, resp >= 3))
+    # correct memory score
+    mutate(score = as.double(xor(old_new == 1, resp >= 3)))
 }
 
 calc_mem_perf <- function(data) {
@@ -10,7 +12,7 @@ calc_mem_perf <- function(data) {
     preproc.iquizoo:::calc_sdt(
       type_signal = 1,
       by = "subj_id",
-      name_acc = "acc",
+      name_acc = "score",
       name_type = "old_new"
     ) |>
     select(subj_id, dprime)
@@ -24,5 +26,5 @@ calc_simil_mem <- function(mem_perf) {
 
 calc_memorability <- function(data) {
   data |>
-    summarise(pc = mean(acc == 1), .by = trial_id)
+    summarise(pc = mean(score == 1), .by = trial_id)
 }
