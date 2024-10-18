b422f070c8f4bafc8ff26c9f1c5eacd37b772903
Liang Zhang <psychelzh@outlook.com>
2024-10-18 12:35:38 +0800

    Ensure missing response is treated as error
    Old word as miss, and new word as false alarm.

--- R/behav.R
+++ R/behav.R
@@ -1,14 +1,14 @@
 calc_mem_perf <- function(data, subjs) {
   data |>
-    mutate(acc = xor(old_new == 1, resp >= 3)) |>
+    mutate(subj_id = match(subj, subjs)) |>
+    filter(!is.na(subj_id)) |>
+    mutate(acc = resp != 0 & xor(old_new == 1, resp >= 3)) |>
     preproc.iquizoo:::calc_sdt(
       type_signal = 1,
-      by = "subj",
+      by = "subj_id",
       name_acc = "acc",
       name_type = "old_new"
     ) |>
-    mutate(subj_id = match(subj, subjs)) |>
-    filter(!is.na(subj_id)) |>
     select(subj_id, dprime)
 }
 
