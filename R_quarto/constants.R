c9ed76bdd40686f766d65d34d1d8484b108af7ed
Liang Zhang <psychelzh@outlook.com>
2025-07-13 22:43:29 +0800

    Move functions to separate files

---
+++ R_quarto/constants.R
@@ -0,0 +1,6 @@
+colors_components <- c("#16607a", "#0b9d86", "#cc734d")
+size_label <- 5
+lab_iss_med <- "ISS\\n(Semantic)"
+lab_igs_med <- "IGS\\n(Fidelity)"
+lab_mem_med <- "Memory Performance"
+theme_set(ggpubr::theme_pubr(base_family = "Gill Sans MT", base_size = 12))
