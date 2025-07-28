bf85056d7031dac811c174e8ab4e86a7a2f64227
Liang Zhang <psychelzh@outlook.com>
2025-07-28 09:17:15 +0800

    Use cISS and cIGS instead of ISS and IGS

--- R_quarto/constants.R
+++ R_quarto/constants.R
@@ -1,5 +1,5 @@
 COLORS_COMPONENTS <- c("#16607a", "#0b9d86", "#cc734d")
 SIZE_LABEL <- 5
-LAB_ISS_MED <- "ISS\\n(Semantic)"
-LAB_IGS_MED <- "IGS\\n(Fidelity)"
+LAB_ISS_MED <- "cISS\\n(Semantic)"
+LAB_IGS_MED <- "cIGS\\n(Fidelity)"
 LAB_MEM_MED <- "Memory Performance"
